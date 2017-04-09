2c8856b6834fc11458b2df51685a87972d632188
hasaki <137447345@qq.com>
2017-04-09 14:55:55 +0800

    单例模式---饱汉

---
+++ src/headfirst/day7/SingleTon1.java
@@ -0,0 +1,16 @@
+package headfirst.day7;
+
+/**
+ * 饱汉模式
+ */
+public class SingleTon1 {
+    private static SingleTon1 singleTon1 = new SingleTon1();
+
+    private SingleTon1() {
+    }
+
+    public static SingleTon1 getInstance() {
+        return singleTon1;
+    }
+
+}
